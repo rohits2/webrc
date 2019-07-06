2660883b3f6ce93c8c70ba2965ca5e68e769bef1
Rohit Singh <singhrohit2@hotmail.com>
2019-07-06 16:38:45 -0600

    Reduced camera resolution

--- camera.py
+++ camera.py
@@ -13,8 +13,8 @@ class VideoCamera(object):
     def __init__(self):
         self.video = cv2.VideoCapture(0)
         logger.info("Opened Camera 0!")
-        self.video.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-        self.video.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        self.video.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+        self.video.set(cv2.CAP_PROP_FRAME_HEIGHT, 160)
         _, image = self.video.read()
         H, W, C = image.shape
         logger.info("Streaming {W}x{H}x{C} frames...".format(W=W, H=H, C=C))
