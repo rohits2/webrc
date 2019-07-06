3c945e5456b4e87fe2d73211d13008d4b3e3718d
Rohit Singh <singhrohit2@hotmail.com>
2019-07-06 16:45:58 -0600

    Add more process stuff

--- camera.py
+++ camera.py
@@ -21,15 +21,19 @@ class VideoCamera(object):
         self.__imbuf = Array('c', H * W * C)
         self.__jpbuf = Array('c', H * W * C * 2)
         self.__jpbufsz = Value('i', 0)
+        self.__frameidx = Value('i', 0)
+        self.__jpegidx = Value('i', 0)
         self.__halt_flag = Value('i', 0)
         self.last_frame = np.frombuffer(self.__imbuf.get_obj(), dtype=np.uint8).reshape(H, W, C)
-        self.last_jpeg = np.frombuffer(self.__jpbuf.get_obj(), dtype=np.uint8).reshape(H * W * C * 2)
+        self.last_jpeg = np.frombuffer(self.__jpbuf.get_obj(), dtype=np.uint8).reshape(H * W * C * 2, 1)
 
         self.img_proc = Process(target=self.__capture)
         self.img_proc.start()
 
         self.encode_proc = Process(target=self.__encode)
         self.encode_proc.start()
+
+        self.last_idx = 0
 
     def __del__(self):
         self.video.release()
@@ -42,16 +46,32 @@ class VideoCamera(object):
         while self.__halt_flag.value == 0:
             _, frame = self.video.read()
             self.last_frame[:, :, :] = frame
+            self.__frameidx.value += 1
 
-    def __encode(self):
+    """def __encode(self):
         while self.__halt_flag.value == 0:
             jpeg_buf = BytesIO()
             img = Image.fromarray(self.last_frame)
             img.save(jpeg_buf, format="jpeg", quality=80, optimize=True, progressive=True)
             jpeg = np.frombuffer(jpeg_buf.getbuffer(), dtype=np.uint8)
             self.__jpbufsz.value = jpeg.shape[0]
-            self.last_jpeg[:jpeg.shape[0]] = jpeg
+            self.last_jpeg[:jpeg.shape[0]] = jpeg"""
+
+    def __encode(self):
+        last_idx = 0
+        while self.__halt_flag.value == 0:
+            while self.__frameidx.value == last_idx:
+                continue
+            ret, jpeg = cv2.imencode('.jpg', self.last_frame)
+            bufsz, _ = jpeg.shape
+            self.__jpbufsz.value = bufsz
+            self.last_jpeg[:bufsz] = jpeg
+            last_idx = self.__frameidx.value
+            self.__jpegidx.value += 1
+
 
     async def get_frame(self):
-        await sleep(0.001)
+        while self.last_idx == self.__jpegidx.value:
+            await sleep(1/30)
+        self.last_idx = self.__jpegidx.value
         return self.last_jpeg[:self.__jpbufsz.value]
